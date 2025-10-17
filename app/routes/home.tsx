fcd6f4dedd318a04a6013ae7295738b4b3237d42
Edward Brotz <edward.brotz@gmail.com>
2025-10-17 11:07:40 -0700

    Drop in a couple of buttons

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,48 @@
+import * as React from 'react'
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+import Typography from '@mui/material/Typography'
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Kurse" },
+    { name: "Kurse", content: "Welcome to Kurse!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <div>
+      <SearchableCardPlane />
+    </div>
+  )
+}
+
+function SearchableCardPlane() {
+  return (
+    <div>
+      <input type="text" placeholder="Search..." />
+      <div className="card-container">
+
+        <Card variant="outlined">
+          <React.Fragment>
+            <CardContent>
+              <Typography variant="h5">Place one</Typography>
+              <Typography variant="body2">1111 1st St</Typography>
+            </CardContent>
+          </React.Fragment>
+        </Card>
+        
+        <Card variant="outlined">
+          <React.Fragment>
+            <CardContent>
+              <Typography variant="h5">Place two</Typography>
+              <Typography variant="body2">2222 2nd St</Typography>
+            </CardContent>
+          </React.Fragment>
+        </Card>
+      </div>
+    </div>
+  );
 }
