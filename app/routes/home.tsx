cd706146b342dca92cd45c1357b823ed88a8bcb3
Edward Brotz <edward.brotz@gmail.com>
2025-10-17 10:55:27 -0700

    Add in initial cards

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,54 @@
+import * as React from 'react'
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Box from '@mui/material/Box'
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+import Grid from '@mui/material/Grid'
+import Typography from '@mui/material/Typography'
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Kurse" },
+    { name: "Kurse", content: "Welcome to Kurse!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <div>
+      <SearchableCardPlane />
+    </div>
+  )
+}
+
+function SearchableCardPlane() {
+  return (
+    <div>
+      <input type="text" placeholder="Search..." />
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={2}>
+            <PlaceCard title="Place one" address="1111 1st St" />
+            <PlaceCard title="Place two" address="2222 2nd St" />
+        </Grid>
+      </Box>
+    </div>
+  );
+}
+
+interface PlaceSummary {
+  title: string;
+  address: string;
+}
+
+function PlaceCard(s: PlaceSummary) {
+  return (
+    <Card variant="outlined" sx={{ maxWidth: 345 }}>
+      <React.Fragment>
+        <CardContent>
+          <Typography variant="h5">{s.title}</Typography>
+          <Typography variant="body2">{s.address}</Typography>
+        </CardContent>
+      </React.Fragment>
+    </Card>
+  );
 }
