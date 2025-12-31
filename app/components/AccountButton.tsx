e7a10de7d59fe0fa570210492954cd22855c7209
Edward Brotz <edward.brotz@gmail.com>
2025-12-31 14:22:42 -0800

    Add AccountButton component

---
+++ app/components/AccountButton.tsx
@@ -0,0 +1,46 @@
+import * as React from 'react';
+import IconButton from '@mui/material/IconButton';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+
+export function Button() {
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+    const open = Boolean(anchorEl);
+
+    const onClickHandler: React.MouseEventHandler<HTMLButtonElement> = (event) => {
+        setAnchorEl(event.currentTarget)
+    }
+
+    const closeHandler = () => {
+        setAnchorEl(null)
+    }
+
+    // TODO Go to user page
+    const myAccountHandler = () => {
+        setAnchorEl(null)
+    }
+
+    // TODO Handle logout
+    const logoutHandler = () => {
+        setAnchorEl(null)
+    }
+
+    return (
+        <IconButton
+            size="large"
+            edge="end"
+            aria-label="account of current user"
+            aria-controls={undefined}
+            aria-haspopup="true"
+            onClick={onClickHandler}
+            color="inherit"
+            >
+            <AccountCircle />
+            <Menu id="account-menu" open={open} onClose={closeHandler}>
+                <MenuItem onClick={myAccountHandler}>My Account</MenuItem>
+                <MenuItem onClick={logoutHandler}>Logout</MenuItem>
+            </Menu>
+        </IconButton>
+    );
+}
